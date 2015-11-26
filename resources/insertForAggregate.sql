dd6421c21c483337fd9356b123019e126c7c0b97
Juan David Sanchez <juancho85@gmail.com>
2015-11-26 09:40:37 +0100

    sql script for inserting values into DB

---
+++ resources/insertForAggregate.sql
@@ -0,0 +1,13 @@
+insert into employee (EID, ENAME, SALARY, DEG)
+values (1201, 'Gopal', 40000, 'Technical Manager');
+insert into employee (EID, ENAME, SALARY, DEG)
+values (1202, 'Manisha', 40000, 'Proof Reader');
+insert into employee (EID, ENAME, SALARY, DEG)
+values (1203, 'Masthanvali', 40000, 'Technical Writer');
+insert into employee (EID, ENAME, SALARY, DEG)
+values (1204, 'Satish', 30000, 'Technical Writer');
+insert into employee (EID, ENAME, SALARY, DEG)
+values (1205, 'Krishna', 30000, 'Technical Writer');
+insert into employee (EID, ENAME, SALARY, DEG)
+values (1206, 'Kiran', 40000, 'Proof Reader');
+
